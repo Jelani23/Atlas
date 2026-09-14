6d5383244899f236a3583c914b9cea504587c308
Jelani23 <100552139+Jelani23@users.noreply.github.com>
2026-09-14 12:51:53 -0500

    Wrap Atlas UI with persistent cloud runtime

--- renderer/app/page.tsx
+++ renderer/app/page.tsx
@@ -1,11 +1,14 @@
 import { AtlasRuntime } from "@/components/atlas-runtime"
+import { CloudRuntime } from "@/components/cloud-runtime"
 import { SkyBackground } from "@/components/sky-background"
 
 export default function Page() {
   return (
     <div className="relative h-dvh overflow-hidden">
       <SkyBackground />
-      <AtlasRuntime />
+      <CloudRuntime>
+        <AtlasRuntime />
+      </CloudRuntime>
     </div>
   )
 }
